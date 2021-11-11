25c0f67e6652e74730653e275d7a351e70611986
Magnus Brantheim <m.brantheim@gmail.com>
2021-11-11 19:37:06 +0100

    Added dotenv and .env file to store secrets

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,15 @@
-require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-waffle")
+require("dotenv").config()
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+  const accounts = await hre.ethers.getSigners()
 
   for (const account of accounts) {
-    console.log(account.address);
+    console.log(account.address)
   }
-});
+})
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -16,14 +17,17 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
- module.exports = {
+module.exports = {
   solidity: "0.8.4",
   paths: {
-    artifacts: './src/artifacts',
+    artifacts: "./src/artifacts",
   },
   networks: {
     hardhat: {
-      chainId: 1337
-    }
-  }
-};
+      forking: {
+        url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.API_KEY}`,
+        blockNumber: 13596490,
+      },
+    },
+  },
+}
