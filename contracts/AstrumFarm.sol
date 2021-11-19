bffaabd977dd73440ea748bf79803f6dfef4846d
Magnus Brantheim <m.brantheim@gmail.com>
2021-11-19 12:49:20 +0100

    Added uniswap liquidity functions with tests

--- contracts/AstrumFarm.sol
+++ contracts/AstrumFarm.sol
@@ -1,59 +1,136 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
 
 import "hardhat/console.sol";
+import {SafeERC20} from "../library/SafeERC20.sol";
+import {IERC20} from "../library/IERC20.sol";
+import {SafeMath} from "../library/SafeMath.sol";
 
 interface IUniswapV2Router02 {
-    function swapETHForExactTokens(uint amountOut, address[] calldata path, address to, uint deadline)
+    function swapETHForExactTokens(
+        uint256 amountOut,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external payable returns (uint256[] memory amounts);
+
+    function getAmountsIn(uint256 amountOut, address[] calldata path) external view returns (uint256[] memory amounts);
+
+    function WETH() external pure returns (address);
+
+    function addLiquidityETH(
+        address token,
+        uint256 amountTokenDesired,
+        uint256 amountTokenMin,
+        uint256 amountETHMin,
+        address to,
+        uint256 deadline
+    )
         external
         payable
-        returns (uint[] memory amounts);
-    function getAmountsIn(uint amountOut, address[] calldata path) external view returns (uint[] memory amounts);
-    function WETH() external pure returns (address);
-}
+        returns (
+            uint256 amountToken,
+            uint256 amountETH,
+            uint256 liquidity
+        );
 
-interface ERC20 {
-    // function totalSupply() external view returns (uint);
-    function balanceOf(address tokenOwner) external view returns (uint balance);
-    // function allowance(address tokenOwner, address spender) external view returns (uint remaining);
-    // function transfer(address to, uint tokens) external returns (bool success);
-    // function approve(address spender, uint tokens) external returns (bool success);
-    // function transferFrom(address from, address to, uint tokens) external returns (bool success);
-
-    // function symbol() external view returns (string memory);
-    // function name() external view returns (string memory);
-    // function decimals() external view returns (uint8);
-
-    // event Transfer(address indexed from, address indexed to, uint tokens);
-    // event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+    function removeLiquidityETH(
+        address token,
+        uint256 liquidity,
+        uint256 amountTokenMin,
+        uint256 amountETHMin,
+        address to,
+        uint256 deadline
+    ) external returns (uint256 amountToken, uint256 amountETH);
 }
 
 contract AstrumFarm {
-  address private constant ROUTER_ADDRESS = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
-  address private constant USDC_ADDRESS = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
-  IUniswapV2Router02 private constant uniswapRouter = IUniswapV2Router02(ROUTER_ADDRESS);
-  ERC20 private constant usdcToken = ERC20(USDC_ADDRESS);
-  
-  constructor() {
-  }
+    using SafeMath for uint256;
+    address private constant ROUTER_ADDRESS = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+    address private constant USDC_ADDRESS = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address private constant USDC_ETH_LP_ADDRESS = 0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc;
+    IUniswapV2Router02 private constant uniswapRouter = IUniswapV2Router02(ROUTER_ADDRESS);
+    IERC20 private constant usdcToken = IERC20(USDC_ADDRESS);
+    IERC20 private constant usdcETHLPToken = IERC20(USDC_ETH_LP_ADDRESS);
+    uint256 constant UNLIMITED_APPROVAL = type(uint256).max;
+    mapping(address => uint256) public balances;
+    event AddLiquidity(address indexed _from, uint256 _liquidity, uint256 _amountToken, uint256 _amountETH);
+    event RemoveLiquidity(address indexed _from, uint256 _liquidity, uint256 _amountToken, uint256 _amountETH);
 
-  function swapETHForExactTokens(uint usdcAmount, uint deadline) public payable {
-    address[] memory path = new address[](2);
-    path[0] = getWETHAddress();
-    path[1] = USDC_ADDRESS;
-    uniswapRouter.swapETHForExactTokens{ value: msg.value }(usdcAmount, path, address(this), deadline);
-  }
-  
-  function getAmountsInETHToUSDC(uint usdcAmount) public view returns (uint[] memory) {
-    address[] memory path = new address[](2);
-    path[0] = getWETHAddress();
-    path[1] = USDC_ADDRESS;
-    return uniswapRouter.getAmountsIn(usdcAmount, path);
-  }
+    constructor() {}
 
-  function getWETHAddress() public pure returns (address) {
-    return uniswapRouter.WETH();
-  }
-  
-  receive() payable external {}
+    function swapETHForExactTokens(uint256 usdcAmount, uint256 deadline) public payable {
+        address[] memory path = new address[](2);
+        path[0] = getWETHAddress();
+        path[1] = USDC_ADDRESS;
+        uniswapRouter.swapETHForExactTokens{value: msg.value}(usdcAmount, path, address(this), deadline);
+        SafeERC20.safeTransfer(usdcToken, msg.sender, usdcAmount);
+    }
+
+    function getAmountsInETHToUSDC(uint256 usdcAmount) public view returns (uint256[] memory) {
+        address[] memory path = new address[](2);
+        path[0] = getWETHAddress();
+        path[1] = USDC_ADDRESS;
+        return uniswapRouter.getAmountsIn(usdcAmount, path);
+    }
+
+    function getWETHAddress() public pure returns (address) {
+        return uniswapRouter.WETH();
+    }
+
+    function approve() public {
+        address spender = address(this);
+        SafeERC20.safeIncreaseAllowance(usdcToken, spender, UNLIMITED_APPROVAL); // Safely set unlimited allowance with USDC
+    }
+
+    function addLiquidityETH(
+        uint256 amountTokenDesired,
+        uint256 amountTokenMin,
+        uint256 amountETHMin,
+        uint256 deadline
+    )
+        external
+        payable
+        returns (
+            uint256 amountToken,
+            uint256 amountETH,
+            uint256 liquidity
+        )
+    {
+        require(msg.value > amountETHMin, "Must send more eth than amountETHMin");
+        SafeERC20.safeTransferFrom(usdcToken, msg.sender, address(this), amountTokenDesired);
+        address spender = ROUTER_ADDRESS;
+        SafeERC20.safeIncreaseAllowance(usdcToken, spender, UNLIMITED_APPROVAL);
+        (amountToken, amountETH, liquidity) = uniswapRouter.addLiquidityETH{value: msg.value}(
+            USDC_ADDRESS,
+            amountTokenDesired,
+            amountTokenMin,
+            amountETHMin,
+            address(this),
+            deadline
+        );
+        balances[msg.sender] = balances[msg.sender].add(liquidity);
+        emit AddLiquidity(msg.sender, liquidity, amountToken, amountETH);
+        return (amountToken, amountETH, liquidity);
+    }
+
+    function removeLiquidityETH(
+        uint256 liquidity,
+        uint256 amountTokenMin,
+        uint256 amountETHMin,
+        uint256 deadline
+    ) external returns (uint256 amountToken, uint256 amountETH) {
+        console.log("heeei");
+        require(balances[msg.sender] >= liquidity, "Attempting to withdraw over the balance for this account");
+        balances[msg.sender] = balances[msg.sender].sub(liquidity);
+        address spender = ROUTER_ADDRESS;
+        SafeERC20.safeIncreaseAllowance(usdcETHLPToken, spender, UNLIMITED_APPROVAL);
+        (amountToken, amountETH) = uniswapRouter.removeLiquidityETH(USDC_ADDRESS, liquidity, amountTokenMin, amountETHMin, msg.sender, deadline);
+        // SafeERC20.safeTransfer(usdcToken, msg.sender, amountToken);
+        //payable(msg.sender).transfer(amountETH);
+        emit RemoveLiquidity(msg.sender, liquidity, amountToken, amountETH);
+        return (amountToken, amountETH);
+    }
+
+    receive() external payable {}
 }
--- test/farm-test.js
+++ test/farm-test.js
@@ -3,38 +3,138 @@ const { ethers } = require("hardhat")
 const { Contract } = require("ethers")
 const ERC20ABI = require("../abi/ERC20.json")
 const provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:8545/")
+const signer = provider.getSigner()
 
 const getUSDCBalance = async (address) => {
-  const usdcAddress = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
-  const contract = new Contract(usdcAddress, ERC20ABI, provider)
-  const balance = await contract.balanceOf(address)
+  const usdc = getUSDCContract()
+  const balance = await usdc.balanceOf(address)
   return balance.toString()
 }
 
+const getUSDCContract = () => {
+  const usdcAddress = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
+  const usdc = new Contract(usdcAddress, ERC20ABI, signer)
+  return usdc
+}
+
+const getDeadline = async (delay) => {
+  const blockNr = await ethers.provider.getBlockNumber()
+  const block = await ethers.provider.getBlock(blockNr)
+  const deadline = block.timestamp + delay
+  return deadline
+}
+
+const resetBlockchain = async () => {
+  await network.provider.request({
+    method: "hardhat_reset",
+    params: [
+      {
+        forking: {
+          jsonRpcUrl: `https://eth-mainnet.alchemyapi.io/v2/${process.env.API_KEY}`,
+          blockNumber: 13596490,
+        },
+      },
+    ],
+  })
+}
+
+const deployFarm = async () => {
+  const Farm = await ethers.getContractFactory("AstrumFarm")
+  const farm = await Farm.deploy()
+  await farm.deployed()
+  return farm
+}
+
 describe("AstrumFarm", () => {
+  beforeEach(async () => {
+    await resetBlockchain()
+  })
   it("Should return amounts in for swapping eth to USDC", async () => {
-    const Farm = await ethers.getContractFactory("AstrumFarm")
-    const farm = await Farm.deploy()
-    await farm.deployed()
+    const farm = await deployFarm()
 
     const amountsIn = await farm.getAmountsInETHToUSDC(100)
     expect(amountsIn).to.be.an("array")
     expect(amountsIn.length).equals(2)
   })
   it("Should swap ETH for USDC", async () => {
-    const Farm = await ethers.getContractFactory("AstrumFarm")
-    const farm = await Farm.deploy()
-    await farm.deployed()
+    const farm = await deployFarm()
+
     const usdcAmount = 100
     const amountsIn = await farm.getAmountsInETHToUSDC(usdcAmount)
-    const blockNr = await ethers.provider.getBlockNumber()
-    const block = await ethers.provider.getBlock(blockNr)
+
     const delay = 1000
-    const deadline = block.timestamp + delay
-    const usdcBalanceBefore = await getUSDCBalance(farm.address)
+    const deadline = getDeadline(delay)
+    const [owner] = await ethers.getSigners()
+    const usdcBalanceBefore = await getUSDCBalance(owner.address)
     expect(Number(usdcBalanceBefore)).equals(0)
+
     await farm.swapETHForExactTokens(amountsIn[1], deadline, { value: amountsIn[0] })
-    const usdcBalanceAfter = await getUSDCBalance(farm.address)
+    const usdcBalanceAfter = await getUSDCBalance(owner.address)
     expect(Number(usdcBalanceAfter)).equals(usdcAmount)
+  })
+  it("Can provide liquidity", async () => {
+    const farm = await deployFarm()
+
+    const usdcAmount = 100
+    const amountsIn = await farm.getAmountsInETHToUSDC(usdcAmount)
+
+    const delay = 1000
+    const deadline = getDeadline(delay)
+    await farm.swapETHForExactTokens(amountsIn[1], deadline, { value: amountsIn[0] })
+
+    const usdc = getUSDCContract()
+    await usdc.approve(farm.address, ethers.constants.MaxInt256)
+
+    const [owner] = await ethers.getSigners()
+    const balanceBefore = await farm.balances(owner.address)
+    expect(Number(balanceBefore)).to.equal(0)
+
+    const allowedSlippage = 0.95
+    const ethAmount = amountsIn[0]
+    await farm.addLiquidityETH(
+      usdcAmount,
+      Math.round(usdcAmount * allowedSlippage),
+      Math.round(ethAmount * allowedSlippage),
+      deadline,
+      { value: ethAmount }
+    )
+    const balanceAfter = await farm.balances(owner.address)
+    expect(Number(balanceAfter)).to.be.greaterThan(0)
+  })
+  it("Can remove liquidity", async () => {
+    const farm = await deployFarm()
+
+    const usdcAmount = 100
+    const amountsIn = await farm.getAmountsInETHToUSDC(usdcAmount)
+
+    const delay = 1000
+    const deadline = getDeadline(delay)
+    await farm.swapETHForExactTokens(amountsIn[1], deadline, { value: amountsIn[0] })
+
+    const usdc = getUSDCContract()
+    await usdc.approve(farm.address, ethers.constants.MaxInt256)
+
+    const [owner] = await ethers.getSigners()
+
+    const allowedSlippage = 0.95
+    const ethAmount = amountsIn[0]
+    await farm.addLiquidityETH(
+      usdcAmount,
+      Math.round(usdcAmount * allowedSlippage),
+      Math.round(ethAmount * allowedSlippage),
+      deadline,
+      { value: ethAmount }
+    )
+    const balanceAfter = await farm.balances(owner.address)
+    const usdcBalanceBefore = Number(await usdc.balanceOf(owner.address))
+    expect(usdcBalanceBefore).to.equal(0)
+    await farm.removeLiquidityETH(
+      balanceAfter,
+      Math.round(usdcAmount * allowedSlippage),
+      Math.round(ethAmount * allowedSlippage),
+      deadline
+    )
+    const usdcBalanceAfter =  Number(await usdc.balanceOf(owner.address))
+    expect(usdcBalanceAfter).to.be.greaterThan(0)
   })
 })
